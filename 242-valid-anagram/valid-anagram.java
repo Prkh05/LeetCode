8f9537e3ecac4bc93c3a5928a2d565a4581b607a
Prakhar Shukla <128119071+Prkh05@users.noreply.github.com>
2025-06-24 22:06:03 +0530

    Time: 13 ms (31.59%) | Memory: 44.8 MB (25.50%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() !=t.length()){
+            return false;
+        } 
+
+        Map<Character,Integer>charCount  =new HashMap<>();
+        
+        for (char c : s.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+
+        for(char c : t.toCharArray()){
+            if(!charCount.containsKey(c)){
+                return false;
+            } 
+            charCount.put(c,charCount.get(c)-1);
+            
+        }
+        for(int count : charCount.values()){
+            if(count!=0){
+                return false;
+            } 
+        }
+
+        return true;
+
+    }
+}
