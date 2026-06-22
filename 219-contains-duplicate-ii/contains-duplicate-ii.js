96396db698fe6c38b9e7b7f8897eaa975ac1b8b6
Prakhar Shukla <128119071+Prkh05@users.noreply.github.com>
2026-06-22 20:19:06 +0530

    Time: 27 ms (60.47%) | Memory: 84.1 MB (46.86%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let map = new Map();
+
+    for(let i =0;i<nums.length;i++){
+        if(map.has(nums[i])){
+            const prevIndex = map.get(nums[i]);
+            if(Math.abs(prevIndex-i)<=k){
+                return true;
+            }
+        }
+        
+        map.set(nums[i],i);
+    }
+    return false;
+
+};
